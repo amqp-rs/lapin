fe9d55cb6f1e4b991dcdf4f96762510df94b4a87
Geoffroy Couprie <geo.couprie@gmail.com>
2017-03-03 16:17:14 +0100

    integrate clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
-#[macro_use] extern crate nom;
-#[macro_use] extern crate rusticata_macros;
+#![cfg_attr(feature="clippy", feature(plugin))]
+#![cfg_attr(feature="clippy", plugin(clippy))]
+
+#[macro_use]
+extern crate nom;
+#[macro_use]
+extern crate rusticata_macros;
 
 pub mod connection;
 pub mod channel;
