6d9d9603ba0a75d855bf8b0a4b981beccdb71780
Geoffroy Couprie <geo.couprie@gmail.com>
2017-03-06 15:49:53 +0100

    implement gen_method_frame

--- src/format/frame.rs
+++ src/format/frame.rs
@@ -103,3 +103,23 @@ pub fn frame(input: &[u8]) -> IResult<&[u8], Frame> {
     },
   }
 }
+
+pub fn gen_method_frame<'a>(input:(&'a mut [u8],usize), channel: u16, class: &Class) -> Result<(&'a mut [u8],usize),GenError> {
+  if let Ok(input1) = gen_be_u8!(input, 1u8) {
+    if let Ok((sl2, index2)) = gen_be_u16!(input1, channel) {
+      if let Ok((sl3, index3)) = gen_class((sl2, index2 + 4), class) {
+        if let Ok(input4) = gen_be_u32!((sl3, index2), index3 - index2 - 4) {
+          gen_be_u8!(input4, 0xCE)
+        } else {
+          Err(GenError::CustomError(42))
+        }
+      } else {
+        Err(GenError::CustomError(42))
+      }
+    } else {
+      Err(GenError::CustomError(42))
+    }
+  } else {
+    Err(GenError::CustomError(42))
+  }
+}
