92780b229fe45d743da88a5319fba1dffebb07cc
Geoffroy Couprie <geo.couprie@gmail.com>
2017-03-15 17:47:25 +0100

    flatten a bit the futures chaining

--- futures/examples/client.rs
+++ futures/examples/client.rs
@@ -10,44 +10,46 @@ use tokio_core::reactor::Core;
 use tokio_core::net::TcpStream;
 
 fn main() {
-      env_logger::init().unwrap();
-      let mut core = Core::new().unwrap();
+  env_logger::init().unwrap();
+  let mut core = Core::new().unwrap();
 
-      let handle = core.handle();
-      let addr = "127.0.0.1:5672".parse().unwrap();
+  let handle = core.handle();
+  let addr = "127.0.0.1:5672".parse().unwrap();
 
-      core.run(
-        TcpStream::connect(&addr, &handle).and_then(|stream| {
-          lapin::client::Client::connect(stream)
-            .and_then(|client| {
-              println!("client exists");
-              client.create_channel().and_then(|channel| {
-                let id = channel.id;
-                println!("created channel with id: {}", id);
-                channel.queue_declare("hello").and_then(move |_| {
-                  println!("channel {} declared queue {}", id, "hello");
-                  channel.basic_publish("hello", b"hello from tokio")
-                })
-              }).and_then(move |_| {
-                client.create_channel().and_then(|channel| {
-                  let id = channel.id;
-                  println!("created channel with id: {}", id);
-                  channel.queue_declare("hello").and_then(move |_| {
-                    println!("channel {} declared queue {}", id, "hello");
-                    channel.basic_consume("hello", "my_consumer").and_then(|stream| {
-                      println!("got consumer stream");
-                      stream.for_each(|message| {
-                        println!("got message: {:?}", message);
-                        println!("decoded message: {:?}", std::str::from_utf8(&message.data).unwrap());
-                        Ok(())
-                      })
+  core.run(
+    TcpStream::connect(&addr, &handle).and_then(|stream| {
+      lapin::client::Client::connect(stream)
+    }).and_then(|client| {
 
-                    })
-                  })
-                })
-              })
-            })
+      client.create_channel().and_then(|channel| {
+        let id = channel.id;
+        println!("created channel with id: {}", id);
+
+        channel.queue_declare("hello").and_then(move |_| {
+          println!("channel {} declared queue {}", id, "hello");
+
+          channel.basic_publish("hello", b"hello from tokio")
         })
-      ).unwrap();
-      panic!();
+      }).and_then(move |_| {
+        client.create_channel()
+      }).and_then(|channel| {
+        let id = channel.id;
+        println!("created channel with id: {}", id);
+
+        channel.queue_declare("hello").and_then(move |_| {
+          println!("channel {} declared queue {}", id, "hello");
+
+          channel.basic_consume("hello", "my_consumer")
+        }).and_then(|stream| {
+          println!("got consumer stream");
+
+          stream.for_each(|message| {
+            println!("got message: {:?}", message);
+            println!("decoded message: {:?}", std::str::from_utf8(&message.data).unwrap());
+            Ok(())
+          })
+        })
+      })
+    })
+  ).unwrap();
 }
