ce93950a23f1306fb76dd41cc237ffd696f0623f
Geoffroy Couprie <geo.couprie@gmail.com>
2017-03-17 11:15:28 +0100

    assets and examples

---
+++ async/assets/test.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var q = 'hello';
+
+    ch.assertQueue(q, {durable: false});
+    // Note: on Node 6 Buffer.from(msg) should be used
+    ch.sendToQueue(q, new Buffer('Hello World!'));
+    console.log(" [x] Sent 'Hello World!'");
+  });
+
+  /*
+  conn.createChannel(function(err, ch) {
+    var q = 'hello';
+
+    ch.assertQueue(q, {durable: false});
+
+    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
+    ch.consume(q, function(msg) {
+      console.log(" [x] Received %s", msg.content.toString());
+    }, {noAck: true});
+  });
+  */
+});
---
+++ async/examples/connection.rs
@@ -0,0 +1,80 @@
+extern crate lapin_async as lapin;
+#[macro_use] extern crate nom;
+
+use std::net::TcpStream;
+use std::iter::repeat;
+use std::io::{Read,Write,Error};
+use std::collections::HashMap;
+use std::{thread,time};
+
+use nom::HexDisplay;
+
+use lapin::connection::*;
+use lapin::method::*;
+use lapin::frame::*;
+use lapin::buffer::Buffer;
+
+fn main() {
+      let mut stream = TcpStream::connect("127.0.0.1:5672").unwrap();
+      stream.set_nonblocking(true);
+
+      let capacity = 8192;
+      let mut send_buffer    = Buffer::with_capacity(capacity as usize);
+      let mut receive_buffer = Buffer::with_capacity(capacity as usize);
+
+      let mut conn: Connection = Connection::new();
+      assert_eq!(conn.connect().unwrap(), ConnectionState::Connecting(ConnectingState::SentProtocolHeader));
+      loop {
+        match conn.run(&mut stream, &mut send_buffer, &mut receive_buffer) {
+          Err(e) => panic!("could not connect: {:?}", e),
+          Ok(ConnectionState::Connected) => break,
+          Ok(state) => println!("now at state {:?}, continue", state),
+        }
+        thread::sleep(time::Duration::from_millis(100));
+      }
+      println!("CONNECTED");
+
+      //now connected
+
+      let channel_a: u16 = conn.create_channel();
+      let channel_b: u16 = conn.create_channel();
+      //send channel
+      conn.channel_open(channel_a, "".to_string()).expect("channel_open");
+      println!("[{}] state: {:?}", line!(), conn.run(&mut stream, &mut send_buffer, &mut receive_buffer).unwrap());
+      thread::sleep(time::Duration::from_millis(100));
+      println!("[{}] state: {:?}", line!(), conn.run(&mut stream, &mut send_buffer, &mut receive_buffer).unwrap());
+
+      //receive channel
+      conn.channel_open(channel_b, "".to_string()).expect("channel_open");
+      println!("[{}] state: {:?}", line!(), conn.run(&mut stream, &mut send_buffer, &mut receive_buffer).unwrap());
+      thread::sleep(time::Duration::from_millis(100));
+      println!("[{}] state: {:?}", line!(), conn.run(&mut stream, &mut send_buffer, &mut receive_buffer).unwrap());
+
+      //create the hello queue
+      conn.queue_declare(channel_a, 0, "hello".to_string(), false, false, false, false, false, HashMap::new()).expect("queue_declare");
+      println!("[{}] state: {:?}", line!(), conn.run(&mut stream, &mut send_buffer, &mut receive_buffer).unwrap());
+      thread::sleep(time::Duration::from_millis(100));
+      println!("[{}] state: {:?}", line!(), conn.run(&mut stream, &mut send_buffer, &mut receive_buffer).unwrap());
+
+      conn.queue_declare(channel_b, 0, "hello".to_string(), false, false, false, false, false, HashMap::new()).expect("queue_declare");
+      println!("[{}] state: {:?}", line!(), conn.run(&mut stream, &mut send_buffer, &mut receive_buffer).unwrap());
+      thread::sleep(time::Duration::from_millis(100));
+      println!("[{}] state: {:?}", line!(), conn.run(&mut stream, &mut send_buffer, &mut receive_buffer).unwrap());
+
+      println!("will consume");
+      conn.basic_consume(channel_b, 0, "hello".to_string(), "my_consumer".to_string(), false, true, false, false, HashMap::new()).expect("basic_consume");
+      println!("[{}] state: {:?}", line!(), conn.run(&mut stream, &mut send_buffer, &mut receive_buffer).unwrap());
+      thread::sleep(time::Duration::from_millis(100));
+      println!("[{}] state: {:?}", line!(), conn.run(&mut stream, &mut send_buffer, &mut receive_buffer).unwrap());
+
+      println!("will publish");
+      conn.basic_publish(channel_a, 0, "".to_string(), "hello".to_string(), false, false).expect("basic_publish");
+      let payload = b"Hello world!";
+      conn.send_content_frames(channel_a, 60, payload);
+      println!("[{}] state: {:?}", line!(), conn.run(&mut stream, &mut send_buffer, &mut receive_buffer).unwrap());
+      thread::sleep(time::Duration::from_millis(100));
+      println!("[{}] state: {:?}", line!(), conn.run(&mut stream, &mut send_buffer, &mut receive_buffer).unwrap());
+      println!("received message: {:?}", conn.next_message(channel_b, "hello", "my_consumer").unwrap());
+      panic!();
+}
+
