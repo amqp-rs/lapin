72ced3bd0300dd64a8dac876216981435b96fbd9
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2017-03-26 15:33:43 +0200

    futures: add properties in publish eample to test them
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- futures/examples/client.rs
+++ futures/examples/client.rs
@@ -30,7 +30,7 @@ fn main() {
         channel.queue_declare("hello", &QueueDeclareOptions::default()).and_then(move |_| {
           info!("channel {} declared queue {}", id, "hello");
 
-          channel.basic_publish("hello", b"hello from tokio", &BasicPublishOptions::default(), BasicProperties::default())
+          channel.basic_publish("hello", b"hello from tokio", &BasicPublishOptions::default(), BasicProperties::default().with_user_id("guest".to_string()).with_reply_to("foobar".to_string()))
         })
       }).and_then(move |_| {
         client.create_channel()
