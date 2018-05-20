ee2508049fffd3fa7e3683923fbbee938325755b
Jeff Parsons <jeff@parsons.io>
2018-05-20 16:39:40 +1000

    Test consuming before publishing

--- futures/tests/connection.rs
+++ futures/tests/connection.rs
@@ -23,7 +23,8 @@ fn amqp_addr() -> SocketAddr {
 
 #[test]
 fn connection() {
-  env_logger::init();
+  // Ignore error initializing logger; other tests might have done it already.
+  let _ = env_logger::try_init();
 
   tokio::run(
     TcpStream::connect(&amqp_addr()).and_then(|stream| {
@@ -71,4 +72,145 @@ fn connection() {
       })
     }).map_err(|_| ())
   )
+}
+
+#[test]
+fn consume_before_publish() {
+  use std::time::{Duration, Instant};
+  use tokio::timer::Delay;
+  use tokio::util::FutureExt;
+
+  // Ignore error initializing logger; other tests might have done it already.
+  let _ = env_logger::try_init();
+
+  let connect_future = TcpStream::connect(&amqp_addr()).and_then(|stream| {
+    lapin::client::Client::connect(stream, &ConnectionOptions::default())
+  });
+
+  tokio::run(
+    connect_future.and_then(|(client, _heartbeat_future_fn)| {
+      // Create a channel and use it to declare and purge a queue for this test.
+      let create_and_purge_queue_future = client.create_channel().and_then(|create_purge_channel| {
+        let id = create_purge_channel.id;
+        info!("created channel with id: {}", id);
+
+        create_purge_channel
+          .queue_declare(
+            "hello_again",
+            &QueueDeclareOptions::default(),
+            &FieldTable::new(),
+          ).and_then(move |_| {
+            // Purge the queue so we don't end up with messages from previous test runs!
+            create_purge_channel.queue_purge(
+              "hello_again",
+              &QueuePurgeOptions::default(),
+            ).map(move |_| {
+              info!("channel {} declared queue {}", id, "hello_again");
+            })
+          })
+      });
+
+      // Create a channel and use it to consume messages.
+      // The result of the future will be a single Delivery.
+      let consume_one_message_future = client.create_channel().and_then(|consume_channel| {
+        let id = consume_channel.id;
+        info!("created channel with id: {}", id);
+        let ack_channel = consume_channel.clone();
+
+        let deadline_when = Instant::now() + Duration::from_millis(1000);
+        consume_channel.basic_consume(
+          "hello_again",
+          "my_consumer",
+          &BasicConsumeOptions::default(),
+          &FieldTable::new(),
+        ).and_then(|stream| {
+          info!("got consumer stream");
+
+          // We've purged the queue, and we're only sending one message below.
+          stream.into_future()
+            .map(|(maybe_message, _stream)| maybe_message.expect("Should have been at least one message..."))
+            .map_err(|(err, _stream)| err)
+        })
+        .and_then(move |message| {
+          info!("got message: {:?}", message);
+          info!("decoded message: {:?}", std::str::from_utf8(&message.data).unwrap());
+          ack_channel.basic_ack(message.delivery_tag).map(move |_| {
+            message
+          })
+        })
+        .deadline(deadline_when)
+        .map_err(|err| {
+          panic!("Didn't receive a message in time: {:?}", err);
+        })
+      });
+
+      // Create channel and use it to publish messages.
+      let publish_future = client.create_channel().and_then(move |publish_channel| {
+        let id = publish_channel.id;
+        info!("created channel with id: {}", id);
+        publish_channel
+          .basic_publish(
+            "",
+            "hello_again",
+            b"please receive me",
+            &BasicPublishOptions::default(),
+            BasicProperties::default()
+              .with_user_id("guest".to_string())
+              .with_reply_to("foobar".to_string()),
+          )
+          .map(|confirmation| {
+            info!(
+              "publish got confirmation: {:?}",
+              confirmation
+            )
+          })
+      });
+
+      // (JP:) Both "Example 1" and "Example 2" below should work,
+      // but don't.
+      //
+      // Prior to some refactoring that I don't believe should
+      // have made any difference, "Example 2" worked. I guess this
+      // is the same issue as the original problem: tasks not
+      // always getting notified when necessary.
+      //
+      // Note that neither of these causes the test to fail;
+      // they just panic in a worker thread. If a solution is found
+      // to the underlying problem, I'll happily write a thorough
+      // test suite demonstrating a bunch of different orders of
+      // operations that should work, structured to be a bit more
+      // easily readable than this. :)
+      //
+      // Maybe if we get lucky (?) then https://github.com/rust-lang/rust/pull/50850
+      // will land first, and all of this will become beautiful!
+
+      // Example 1: Consume before publish.
+      create_and_purge_queue_future.and_then({|_|
+        consume_one_message_future.and_then(|_| {
+          // Wait a little bit to make sure that we really
+          // do start consuming first.
+          let when = Instant::now() + Duration::from_millis(100);
+          Delay::new(when)
+            .map_err(|e| panic!("timer failed; err={:?}", e))
+            .and_then(move |_| {
+              publish_future
+            })
+        })
+      })
+
+      // Example 2: Publish before consume.
+      // create_and_purge_queue_future.and_then(|_| {
+      //   publish_future.and_then(|_| {
+      //     let when = Instant::now() + Duration::from_millis(100);
+      //     Delay::new(when)
+      //       .map_err(|e| panic!("timer failed; err={:?}", e))
+      //       .and_then(move |_| {
+      //         consume_one_message_future.map(|_| ())
+      //       })
+      //   })
+      // })
+    }).map_err(|err| {
+      panic!("uh oh: {:?}", err);
+    })
+  );
 }
