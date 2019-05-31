17923d2b36fcf4b2f018134bcc5cc18e0a98ca87
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2019-05-31 10:43:57 +0200

    make basic_publish low prio
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- async/templates/channel.rs
+++ async/templates/channel.rs
@@ -110,7 +110,7 @@ impl Channel {
       {{/if ~}}
     };
 
-    self.send_method_frame(Priority::NORMAL, method, expected_reply)?;
+    self.send_method_frame(Priority::{{#if method.metadata.priority ~}}{{method.metadata.priority}}{{else}}NORMAL{{/if ~}}, method, expected_reply)?;
 
     {{#if method.metadata.end_hook ~}}
     {{#if method.metadata.end_hook.return_type ~}}let end_hook_ret = {{/if ~}}self.on_{{snake class.name false}}_{{snake method.name false}}_sent({{#each method.metadata.end_hook.params as |param| ~}}{{#unless @first ~}}, {{/unless ~}}{{param}}{{/each ~}})?;
