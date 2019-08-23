596257b295134cffe43c19c0cc2d0545d6ccb508
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2019-08-23 10:26:03 +0200

    add example of custom tls connection
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

---
+++ examples/custom_tls_connection.rs
@@ -0,0 +1,117 @@
+use amq_protocol::tcp::AMQPUriTcpExt;
+use env_logger;
+use lapin::{
+    confirmation::Confirmation, message::Delivery, options::*, types::FieldTable, BasicProperties,
+    Connection, ConnectionProperties, ConsumerDelegate, Error,
+};
+use log::info;
+use tcp_stream::{HandshakeError, NativeTlsConnector};
+
+#[derive(Clone, Debug, PartialEq)]
+struct Subscriber;
+
+impl ConsumerDelegate for Subscriber {
+    fn on_new_delivery(&self, delivery: Delivery) {
+        info!("received message: {:?}", delivery);
+    }
+}
+
+fn connect() -> Result<Connection, Error> {
+    // You need to use amqp:// scheme here to handle the tls part manually as it's automatic when you use amqps
+    let addr = std::env::var("AMQP_ADDR").unwrap_or_else(|_| "amqp://127.0.0.1:5672/%2f".into());
+    let (conn, io_loop) = addr
+        .connect(|stream, uri| {
+            let tls_builder = NativeTlsConnector::builder();
+            // Perform here your custom tls setup, with tls_builder.identity or whatever else you need
+            let mut res = stream.into_native_tls(
+                tls_builder.build().expect("TLS configuration failed"),
+                &uri.authority.host,
+            );
+            while let Err(error) = res {
+                match error {
+                    HandshakeError::Failure(io_err) => {
+                        panic!("TLS connection failed: {:?}", io_err)
+                    }
+                    HandshakeError::WouldBlock(mid) => res = mid.handshake(),
+                }
+            }
+            let stream = res.unwrap();
+            Connection::connector(ConnectionProperties::default())(stream, uri)
+        })
+        .map_err(Error::IOError)??;
+    io_loop.run()?;
+    Confirmation::from(Ok(conn)).wait()
+}
+
+fn main() {
+    std::env::set_var("RUST_LOG", "trace");
+
+    env_logger::init();
+
+    let conn = connect().expect("connection error");
+
+    info!("CONNECTED");
+
+    //now connected
+
+    //send channel
+    let channel_a = conn.create_channel().wait().expect("create_channel");
+    //receive channel
+    let channel_b = conn.create_channel().wait().expect("create_channel");
+    info!("[{}] state: {:?}", line!(), conn.status().state());
+
+    //create the hello queue
+    let queue = channel_a
+        .queue_declare(
+            "hello",
+            QueueDeclareOptions::default(),
+            FieldTable::default(),
+        )
+        .wait()
+        .expect("queue_declare");
+    info!("[{}] state: {:?}", line!(), conn.status().state());
+    info!("[{}] declared queue: {:?}", line!(), queue);
+
+    channel_a
+        .confirm_select(ConfirmSelectOptions::default())
+        .wait()
+        .expect("confirm_select");
+    info!("[{}] state: {:?}", line!(), conn.status().state());
+
+    let queue = channel_b
+        .queue_declare(
+            "hello",
+            QueueDeclareOptions::default(),
+            FieldTable::default(),
+        )
+        .wait()
+        .expect("queue_declare");
+    info!("[{}] state: {:?}", line!(), conn.status().state());
+
+    info!("will consume");
+    channel_b
+        .basic_consume(
+            &queue,
+            "my_consumer",
+            BasicConsumeOptions::default(),
+            FieldTable::default(),
+        )
+        .wait()
+        .expect("basic_consume")
+        .set_delegate(Box::new(Subscriber));
+    info!("[{}] state: {:?}", line!(), conn.status().state());
+
+    info!("will publish");
+    let payload = b"Hello world!";
+    channel_a
+        .basic_publish(
+            "",
+            "hello",
+            BasicPublishOptions::default(),
+            payload.to_vec(),
+            BasicProperties::default(),
+        )
+        .wait()
+        .expect("basic_publish");
+    info!("[{}] state: {:?}", line!(), conn.status().state());
+}
