2e1237a8cc13f8c2343b922ccda48d0646e83a16
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-03-24 09:10:03 +0100

    rename examples
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

---
+++ examples/pubsub_nofutures.rs
@@ -0,0 +1,74 @@
+use lapin::{
+    message::DeliveryResult, options::*, types::FieldTable, BasicProperties, Channel, Connection,
+    ConnectionProperties, ConsumerDelegate,
+};
+use log::info;
+
+#[derive(Clone, Debug)]
+struct Subscriber {
+    channel: Channel,
+}
+
+impl ConsumerDelegate for Subscriber {
+    fn on_new_delivery(&self, delivery: DeliveryResult) {
+        if let Ok(Some(delivery)) = delivery {
+            self.channel
+                .basic_ack(delivery.delivery_tag, BasicAckOptions::default())
+                .wait()
+                .expect("basic_ack");
+        }
+    }
+}
+
+fn main() {
+    env_logger::init();
+
+    let addr = std::env::var("AMQP_ADDR").unwrap_or_else(|_| "amqp://127.0.0.1:5672/%2f".into());
+    let conn = Connection::connect(&addr, ConnectionProperties::default())
+        .wait()
+        .expect("connection error");
+
+    info!("CONNECTED");
+
+    let channel_a = conn.create_channel().wait().expect("create_channel");
+    let channel_b = conn.create_channel().wait().expect("create_channel");
+
+    let queue = channel_a
+        .queue_declare(
+            "hello",
+            QueueDeclareOptions::default(),
+            FieldTable::default(),
+        )
+        .wait()
+        .expect("queue_declare");
+
+    info!("Declared queue {:?}", queue);
+
+    info!("will consume");
+    channel_b
+        .clone()
+        .basic_consume(
+            "hello",
+            "my_consumer",
+            BasicConsumeOptions::default(),
+            FieldTable::default(),
+        )
+        .wait()
+        .expect("basic_consume")
+        .set_delegate(Box::new(Subscriber { channel: channel_b }));
+
+    let payload = b"Hello world!";
+
+    loop {
+        channel_a
+            .basic_publish(
+                "",
+                "hello",
+                BasicPublishOptions::default(),
+                payload.to_vec(),
+                BasicProperties::default(),
+            )
+            .wait()
+            .expect("basic_publish");
+    }
+}
