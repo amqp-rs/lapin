6f96369db61644f166cff7f5cad5bd63a543fcc7
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-03-31 16:00:28 +0200

    don't store unneeded promises
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/returned_messages.rs
+++ src/returned_messages.rs
@@ -43,24 +43,35 @@ impl ReturnedMessages {
     }
 
     pub(crate) fn register_dropped_confirm(&self, promise: PinkySwear<Confirmation>) {
-        self.inner.lock().dropped_confirms.push(promise);
+        self.inner.lock().register_dropped_confirm(promise);
     }
 }
 
 #[derive(Debug, Default)]
 pub struct Inner {
     current_message: Option<BasicReturnMessage>,
-    messages: VecDeque<BasicReturnMessage>,
+    waiting_messages: VecDeque<BasicReturnMessage>,
+    messages: Vec<BasicReturnMessage>,
     dropped_confirms: Vec<PinkySwear<Confirmation>>,
     pinkies: VecDeque<PinkyBroadcaster<Confirmation>>,
 }
 
 impl Inner {
     fn register_pinky(&mut self, pinky: PinkyBroadcaster<Confirmation>) {
-        if let Some(message) = self.messages.pop_front() {
+        if let Some(message) = self.waiting_messages.pop_front() {
             pinky.swear(Confirmation::Nack(message));
         } else {
             self.pinkies.push_back(pinky);
+        }
+    }
+
+    fn register_dropped_confirm(&mut self, promise: PinkySwear<Confirmation>) {
+        if let Some(confirmation) = promise.try_wait() {
+            if let Confirmation::Nack(message) = confirmation {
+                self.messages.push(message);
+            }
+        } else {
+            self.dropped_confirms.push(promise);
         }
     }
 
@@ -70,13 +81,13 @@ impl Inner {
             if let Some(pinky) = self.pinkies.pop_front() {
                 pinky.swear(Confirmation::Nack(message));
             } else {
-                self.messages.push_back(message);
+                self.waiting_messages.push_back(message);
             }
         }
     }
 
     fn drain(&mut self) -> Vec<BasicReturnMessage> {
-        let mut messages = Vec::default();
+        let mut messages = self.messages.drain(..).collect::<Vec<BasicReturnMessage>>();
         for promise in self
             .dropped_confirms
             .drain(..)
