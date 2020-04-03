ff5575e9573d9c6a048a036fe8eba0410530804d
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-04-03 12:21:57 +0200

    add CloseOnDrop::into_inner
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/close_on_drop.rs
+++ src/close_on_drop.rs
@@ -2,12 +2,17 @@ use std::ops::Deref;
 
 #[derive(Debug)]
 pub struct CloseOnDrop<T: __private::Closable> {
-    inner: T,
+    inner: Option<T>,
 }
 
 impl<T: __private::Closable> CloseOnDrop<T> {
     pub fn new(inner: T) -> Self {
-        Self { inner }
+        Self { inner: Some(inner) }
+    }
+
+    /// Give the underlying object, cancelling the close on drop action
+    pub fn into_inner(mut self) -> T {
+        self.inner.take().expect("inner should only be None once consumed or dropped")
     }
 }
 
@@ -15,13 +20,15 @@ impl<T: __private::Closable> Deref for CloseOnDrop<T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
-        &self.inner
+        self.inner.as_ref().expect("inner should only be None once consumed or dropped")
     }
 }
 
 impl<T: __private::Closable> Drop for CloseOnDrop<T> {
     fn drop(&mut self) {
-        self.close();
+        if let Some(inner) = self.inner.as_ref() {
+            inner.close();
+        }
     }
 }
 
