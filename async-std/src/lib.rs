172d365694e9ffa7da4915183876b08a40465aa6
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-04-11 17:58:32 +0200

    first draft of AsyncStdExecutor
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- async-std/src/lib.rs
+++ async-std/src/lib.rs
@@ -1,7 +1,16 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use lapin::{
+    executor::Executor,
+    Result,
+};
+
+#[derive(Debug)]
+pub struct AsyncStdExecutor;
+
+impl Executor for AsyncStdExecutor {
+    fn execute(&self, f: Box<dyn FnOnce() + Send>) -> Result<()> {
+        async_std::task::spawn(async {
+            f();
+        });
+        Ok(())
     }
 }
