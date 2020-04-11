97487a176125543848beebac0043d294d8f0462a
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-04-11 22:08:11 +0200

    implement async-amqp
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- async-std/src/lib.rs
+++ async-std/src/lib.rs
@@ -1,10 +1,21 @@
-use lapin::{
-    executor::Executor,
-    Result,
-};
+use lapin::{executor::Executor, ConnectionProperties, Result};
+use std::sync::Arc;
+
+pub trait LapinAsyncStdExt {
+    fn with_async_std(self) -> Self
+    where
+        Self: Sized;
+}
+
+impl LapinAsyncStdExt for ConnectionProperties {
+    fn with_async_std(mut self) -> Self {
+        self.executor = Some(Arc::new(AsyncStdExecutor));
+        self
+    }
+}
 
 #[derive(Debug)]
-pub struct AsyncStdExecutor;
+struct AsyncStdExecutor;
 
 impl Executor for AsyncStdExecutor {
     fn execute(&self, f: Box<dyn FnOnce() + Send>) -> Result<()> {
