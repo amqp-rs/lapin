90259821e04d721358a07a8e4d666b24c52dd5ae
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-04-11 22:05:51 +0200

    implement tokio-amqp
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- tokio/src/lib.rs
+++ tokio/src/lib.rs
@@ -1,16 +1,25 @@
-use lapin::{
-    executor::Executor,
-    Result,
-};
+use lapin::{executor::Executor, ConnectionProperties, Result};
+use std::sync::Arc;
+
+pub trait LapinTokioExt {
+    fn with_tokio(self) -> Self
+    where
+        Self: Sized;
+}
+
+impl LapinTokioExt for ConnectionProperties {
+    fn with_tokio(mut self) -> Self {
+        self.executor = Some(Arc::new(TokioExecutor));
+        self
+    }
+}
 
 #[derive(Debug)]
-pub struct TokioExecutor;
+struct TokioExecutor;
 
 impl Executor for TokioExecutor {
     fn execute(&self, f: Box<dyn FnOnce() + Send>) -> Result<()> {
-        tokio::spawn(async {
-            f()
-        });
+        tokio::spawn(async { f() });
         Ok(())
     }
 }
