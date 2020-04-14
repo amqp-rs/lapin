ae070ec768bfd63c34724b83e5a5e604a7a179ff
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-04-14 21:44:03 +0200

    implement lapinou
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- lapinou/src/lib.rs
+++ lapinou/src/lib.rs
@@ -1,7 +1,25 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use lapin::{executor::Executor, ConnectionProperties, Result};
+use smol::Task;
+use std::{future::Future, pin::Pin};
+
+pub trait LapinSmolExt {
+    fn with_smol(self) -> Self
+    where
+        Self: Sized;
+}
+
+impl LapinSmolExt for ConnectionProperties {
+    fn with_smol(self) -> Self {
+        self.with_executor(SmolExecutor)
+    }
+}
+
+#[derive(Debug)]
+struct SmolExecutor;
+
+impl Executor for SmolExecutor {
+    fn spawn(&self, f: Pin<Box<dyn Future<Output = ()> + Send>>) -> Result<()> {
+        Task::spawn(f).detach();
+        Ok(())
     }
 }
