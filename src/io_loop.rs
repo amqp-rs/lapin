91b797158cc4db8fa1ee59bc63007bf3559eda52
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-04-17 14:09:48 +0200

    flush the socket more often
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/io_loop.rs
+++ src/io_loop.rs
@@ -193,6 +193,7 @@ impl<T: Source + Read + Write + Send + 'static> IoLoop<T> {
         trace!("io_loop run");
         self.ensure_setup()?;
         self.poll(events)?;
+        self.socket.flush()?;
         self.do_run()
     }
 
