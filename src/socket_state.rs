ed1ff3f2c344062906b611d8d2769195a4f84e69
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-04-18 11:34:53 +0200

    introduce SocketState + SocketStateHandle helpers
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

---
+++ src/socket_state.rs
@@ -0,0 +1,117 @@
+use crate::Result;
+use crossbeam_channel::{Receiver, Sender};
+use log::trace;
+
+pub(crate) struct SocketState {
+    readable: bool,
+    writable: bool,
+    error: bool,
+    closed: bool,
+    events: Receiver<SocketEvent>,
+    handle: SocketStateHandle,
+}
+
+impl Default for SocketState {
+    fn default() -> Self {
+        let (sender, receiver) = crossbeam_channel::unbounded();
+        Self {
+            readable: true,
+            writable: true,
+            error: false,
+            closed: false,
+            events: receiver,
+            handle: SocketStateHandle { sender },
+        }
+    }
+}
+
+#[derive(Clone)]
+pub(crate) struct SocketStateHandle {
+    sender: Sender<SocketEvent>,
+}
+
+#[derive(Debug)]
+pub(crate) enum SocketEvent {
+    Readable,
+    Writable,
+    Error,
+    Closed,
+    Wake,
+}
+
+impl SocketState {
+    pub(crate) fn readable(&mut self) -> bool {
+        self.readable
+    }
+
+    pub(crate) fn writable(&mut self) -> bool {
+        self.writable
+    }
+
+    pub(crate) fn error(&mut self) -> bool {
+        self.error
+    }
+
+    pub(crate) fn closed(&mut self) -> bool {
+        self.closed
+    }
+
+    pub(crate) fn wait(&mut self) {
+        self.handle_event(self.events.recv().expect("waiting for socket event failed"))
+    }
+
+    pub(crate) fn handle_read_result(&mut self, result: Result<()>) -> Result<()> {
+        self.readable = Self::handle_io_result(result)?;
+        Ok(())
+    }
+
+    pub(crate) fn handle_write_result(&mut self, result: Result<()>) -> Result<()> {
+        self.writable = Self::handle_io_result(result)?;
+        Ok(())
+    }
+
+    pub(crate) fn handle(&self) -> SocketStateHandle {
+        self.handle.clone()
+    }
+
+    fn handle_io_result(result: Result<()>) -> Result<bool> {
+        if let Err(err) = result {
+            if err.wouldblock() {
+                Ok(false)
+            } else {
+                Err(err)
+            }
+        } else {
+            Ok(true)
+        }
+    }
+
+    pub(crate) fn poll_events(&mut self) {
+        while let Ok(event) = self.events.try_recv() {
+            self.handle_event(event);
+        }
+    }
+
+    fn handle_event(&mut self, event: SocketEvent) {
+        trace!("Got event for socket: {:?}", event);
+        match event {
+            SocketEvent::Readable => self.readable = true,
+            SocketEvent::Writable => self.writable = true,
+            SocketEvent::Error => self.error = true,
+            SocketEvent::Closed => self.closed = true,
+            SocketEvent::Wake => {}
+        }
+    }
+}
+
+impl SocketStateHandle {
+    pub(crate) fn send(&self, event: SocketEvent) {
+        self.sender
+            .send(event)
+            .expect("forwarding soket event failed")
+    }
+
+    pub(crate) fn wake(&self) {
+        self.send(SocketEvent::Wake);
+    }
+}
