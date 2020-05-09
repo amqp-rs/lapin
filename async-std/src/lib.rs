760f62b03726e61c964e015c3fdfcc5d94644e4c
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-05-09 14:55:08 +0200

    async-amqp: use lapinou for reactor
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- async-std/src/lib.rs
+++ async-std/src/lib.rs
@@ -1,4 +1,5 @@
 use lapin::{executor::Executor, ConnectionProperties, Result};
+use lapinou::LapinSmolExt;
 use std::{future::Future, pin::Pin};
 
 pub trait LapinAsyncStdExt {
@@ -9,15 +10,24 @@ pub trait LapinAsyncStdExt {
     fn with_async_std_executor(self) -> Self
     where
         Self: Sized;
+
+    fn with_async_std_reactor(self) -> Self
+    where
+        Self: Sized;
 }
 
 impl LapinAsyncStdExt for ConnectionProperties {
     fn with_async_std(self) -> Self {
-        self.with_async_std_executor()
+        self.with_async_std_executor().with_async_std_reactor()
     }
 
     fn with_async_std_executor(self) -> Self {
         self.with_executor(AsyncStdExecutor)
+    }
+
+    fn with_async_std_reactor(self) -> Self {
+        // async-std uses smol underneath, use smol reactor until async-std exposes its own API
+        self.with_smol_reactor()
     }
 }
 
