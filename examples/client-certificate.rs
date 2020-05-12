0735ab75fa21677f7eb897ba8171cf7f7b0f027c
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-05-12 16:37:51 +0200

    add example with client-certificate usage
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

---
+++ examples/client-certificate.rs
@@ -0,0 +1,108 @@
+use futures_executor::LocalPool;
+use lapin::{
+    message::DeliveryResult,
+    options::*,
+    publisher_confirm::Confirmation,
+    tcp::{Identity, TLSConfig},
+    types::FieldTable,
+    BasicProperties, Connection, ConnectionProperties,
+};
+use log::info;
+use std::sync::Arc;
+
+fn get_tls_config() -> TLSConfig<'static, 'static, 'static> {
+    let cert_chain = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/path/to/ca_certificate.pem"));
+    let client_cert_and_key = include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/path/to/client.pfx"));
+    let client_cert_and_key_password = "bunnies";
+
+    TLSConfig {
+        identity: Some(Identity {
+            der: client_cert_and_key,
+            password: client_cert_and_key_password,
+        }),
+        cert_chain,
+    }
+}
+
+fn main() {
+    env_logger::init();
+
+    let addr = std::env::var("AMQP_ADDR")
+        .unwrap_or_else(|_| "amqps://localhost:5671/%2f?auth_mechanism=external".into());
+
+    LocalPool::new().run_until(async {
+        let conn = Connection::connect_with_config(
+            &addr,
+            ConnectionProperties::default(),
+            get_tls_config(),
+        )
+        .await
+        .expect("connection error");
+
+        info!("CONNECTED");
+
+        //send channel
+        let channel_a = conn.create_channel().await.expect("create_channel");
+        //receive channel
+        let channel_b = conn
+            .create_channel()
+            .await
+            .expect("create_channel")
+            .into_inner();
+        info!("[{}] state: {:?}", line!(), conn.status().state());
+
+        //create the hello queue
+        let queue = channel_a
+            .queue_declare(
+                "hello",
+                QueueDeclareOptions::default(),
+                FieldTable::default(),
+            )
+            .await
+            .expect("queue_declare");
+        info!("[{}] state: {:?}", line!(), conn.status().state());
+        info!("[{}] declared queue: {:?}", line!(), queue);
+
+        let channel_b = Arc::new(channel_b);
+        let chan = channel_b.clone();
+        info!("will consume");
+        channel_b
+            .basic_consume(
+                "hello",
+                "my_consumer",
+                BasicConsumeOptions::default(),
+                FieldTable::default(),
+            )
+            .await
+            .expect("basic_consume")
+            .set_delegate(move |delivery: DeliveryResult| {
+                let chan = chan.clone();
+                async move {
+                    info!("received message: {:?}", delivery);
+                    if let Ok(Some(delivery)) = delivery {
+                        chan.basic_ack(delivery.delivery_tag, BasicAckOptions::default())
+                            .await
+                            .expect("basic_ack");
+                    }
+                }
+            });
+        info!("[{}] state: {:?}", line!(), conn.status().state());
+
+        info!("will publish");
+        let payload = b"Hello world!";
+        let confirm = channel_a
+            .basic_publish(
+                "",
+                "hello",
+                BasicPublishOptions::default(),
+                payload.to_vec(),
+                BasicProperties::default(),
+            )
+            .await
+            .expect("basic_publish")
+            .await
+            .expect("publisher-confirms");
+        assert_eq!(confirm, Confirmation::NotRequested);
+        info!("[{}] state: {:?}", line!(), conn.status().state());
+    })
+}
