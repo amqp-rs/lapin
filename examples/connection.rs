8186b717ac81dc98808a09128c191f2d9354731d
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-05-17 10:00:29 +0200

    examples: use conneciton.run in connection
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- examples/connection.rs
+++ examples/connection.rs
@@ -4,10 +4,10 @@ use lapin::{
     BasicProperties, Connection, ConnectionProperties,
 };
 use log::info;
-use std::{sync::Arc, time::Duration};
+use std::sync::Arc;
 
 fn main() {
-    std::env::set_var("RUST_LOG", "info");
+    std::env::set_var("RUST_LOG", "trace");
 
     env_logger::init();
 
@@ -20,69 +20,71 @@ fn main() {
 
         info!("CONNECTED");
 
-        //send channel
-        let channel_a = conn.create_channel().await.expect("create_channel");
-        //receive channel
-        let channel_b = conn.create_channel().await.expect("create_channel");
-        info!("[{}] state: {:?}", line!(), conn.status().state());
+        {
+            //send channel
+            let channel_a = conn.create_channel().await.expect("create_channel");
+            //receive channel
+            let channel_b = conn.create_channel().await.expect("create_channel");
+            info!("[{}] state: {:?}", line!(), conn.status().state());
 
-        //create the hello queue
-        let queue = channel_a
-            .queue_declare(
-                "hello",
-                QueueDeclareOptions::default(),
-                FieldTable::default(),
-            )
-            .await
-            .expect("queue_declare");
-        info!("[{}] state: {:?}", line!(), conn.status().state());
-        info!("[{}] declared queue: {:?}", line!(), queue);
+            //create the hello queue
+            let queue = channel_a
+                .queue_declare(
+                    "hello",
+                    QueueDeclareOptions::default(),
+                    FieldTable::default(),
+                )
+                .await
+                .expect("queue_declare");
+            info!("[{}] state: {:?}", line!(), conn.status().state());
+            info!("[{}] declared queue: {:?}", line!(), queue);
 
-        let channel_b = Arc::new(channel_b);
-        let chan = channel_b.clone();
-        info!("will consume");
-        channel_b
-            .basic_consume(
-                "hello",
-                "my_consumer",
-                BasicConsumeOptions::default(),
-                FieldTable::default(),
-            )
-            .await
-            .expect("basic_consume")
-            .set_delegate(move |delivery: DeliveryResult| {
-                let chan = chan.clone();
-                async move {
-                    info!("received message: {:?}", delivery);
-                    if let Ok(Some(delivery)) = delivery {
-                        chan.basic_ack(delivery.delivery_tag, BasicAckOptions::default())
-                            .await
-                            .expect("basic_ack");
-                        chan.basic_cancel("my_consumer", BasicCancelOptions::default())
-                            .await
-                            .expect("basic_cancel");
+            let channel_b = Arc::new(channel_b);
+            let chan = channel_b.clone();
+            info!("will consume");
+            channel_b
+                .basic_consume(
+                    "hello",
+                    "my_consumer",
+                    BasicConsumeOptions::default(),
+                    FieldTable::default(),
+                )
+                .await
+                .expect("basic_consume")
+                .set_delegate(move |delivery: DeliveryResult| {
+                    let chan = chan.clone();
+                    async move {
+                        info!("received message: {:?}", delivery);
+                        if let Ok(Some(delivery)) = delivery {
+                            chan.basic_ack(delivery.delivery_tag, BasicAckOptions::default())
+                                .await
+                                .expect("basic_ack");
+                            chan.basic_cancel("my_consumer", BasicCancelOptions::default())
+                                .await
+                                .expect("basic_cancel");
+                        }
                     }
-                }
-            });
-        info!("[{}] state: {:?}", line!(), conn.status().state());
+                });
+            info!("[{}] state: {:?}", line!(), conn.status().state());
 
-        info!("will publish");
-        let payload = b"Hello world!";
-        let confirm = channel_a
-            .basic_publish(
-                "",
-                "hello",
-                BasicPublishOptions::default(),
-                payload.to_vec(),
-                BasicProperties::default(),
-            )
-            .await
-            .expect("basic_publish")
-            .await
-            .expect("publisher-confirms");
-        assert_eq!(confirm, Confirmation::NotRequested);
-        info!("[{}] state: {:?}", line!(), conn.status().state());
+            info!("will publish");
+            let payload = b"Hello world!";
+            let confirm = channel_a
+                .basic_publish(
+                    "",
+                    "hello",
+                    BasicPublishOptions::default(),
+                    payload.to_vec(),
+                    BasicProperties::default(),
+                )
+                .await
+                .expect("basic_publish")
+                .await
+                .expect("publisher-confirms");
+            assert_eq!(confirm, Confirmation::NotRequested);
+            info!("[{}] state: {:?}", line!(), conn.status().state());
+        }
+
+        conn.run().expect("conn.run");
     });
-
-    std::thread::sleep(Duration::from_secs(2));
 }
