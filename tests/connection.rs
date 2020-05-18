8576e1f199b14b7e5300c94f0ceb71a883cf648a
Gustavo Sampaio <gbritosampaio@gmail.com>
2020-05-17 22:27:18 -0300

    Add test case

--- tests/connection.rs
+++ tests/connection.rs
@@ -113,3 +113,82 @@ fn connection() {
     thread::sleep(time::Duration::from_millis(100));
     assert!(hello_world.load(Ordering::SeqCst));
 }
+
+#[test]
+fn test_spawn() {
+    let _ = env_logger::try_init();
+
+    let addr = std::env::var("AMQP_ADDR").unwrap_or_else(|_| "amqp://127.0.0.1:5672/%2f".into());
+    let conn = Connection::connect(&addr, ConnectionProperties::default())
+        .wait()
+        .expect("connection error");
+
+    println!("CONNECTED with configuration: {:?}", conn.configuration());
+
+    //now connected
+
+    //send channel
+    let channel_a = conn.create_channel().wait().expect("create_channel");
+    //receive channel
+    let channel_b = conn.create_channel().wait().expect("create_channel");
+
+    //create the hello queue
+    let queue = channel_a
+        .queue_declare(
+            "hello-async",
+            QueueDeclareOptions::default(),
+            FieldTable::default(),
+        )
+        .wait()
+        .expect("queue_declare");
+    println!("[{}] state: {:?}", line!(), conn.status().state());
+    println!("[{}] declared queue: {:?}", line!(), queue);
+
+    //purge the hello queue in case it already exists with contents in it
+    let queue = channel_a
+        .queue_purge("hello-async", QueuePurgeOptions::default())
+        .wait()
+        .expect("queue_purge");
+    println!("[{}] state: {:?}", line!(), conn.status().state());
+    info!("Declared queue {:?}", queue);
+
+    println!("will publish");
+    let payload = b"Hello world!";
+    let confirm = channel_a
+        .basic_publish(
+            "",
+            "hello-async",
+            BasicPublishOptions::default(),
+            payload.to_vec(),
+            BasicProperties::default(),
+        )
+        .wait()
+        .expect("basic_publish")
+        .wait()
+        .expect("publisher-confirms");
+    assert_eq!(confirm, Confirmation::NotRequested);
+    println!("[{}] state: {:?}", line!(), conn.status().state());
+
+    let hello_world = Arc::new(AtomicBool::new(false));
+    let subscriber = Subscriber {
+        hello_world: hello_world.clone(),
+    };
+    println!("will consume");
+    let consumer = channel_b
+        .basic_consume(
+            "hello-async",
+            "my_consumer",
+            BasicConsumeOptions::default(),
+            FieldTable::default(),
+        )
+        .wait()
+        .expect("basic_consume");
+
+    thread::sleep(time::Duration::from_secs(1));
+
+    consumer.set_delegate(subscriber);
+    println!("[{}] state: {:?}", line!(), conn.status().state());
+
+    thread::sleep(time::Duration::from_millis(100));
+    assert!(hello_world.load(Ordering::SeqCst));
+}
