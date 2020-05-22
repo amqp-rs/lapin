4918b7809774d10b015451963ef4d1731d879027
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-05-22 10:33:20 +0200

    async-amqp: don't depend on async-std 1.6 for now
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- async-std/src/lib.rs
+++ async-std/src/lib.rs
@@ -1,5 +1,5 @@
 use lapin::{executor::Executor, ConnectionProperties, Result};
-use lapinou::LapinSmolExt;
+//use lapinou::LapinSmolExt;
 use std::{future::Future, pin::Pin};
 
 pub trait LapinAsyncStdExt {
@@ -11,24 +11,24 @@ pub trait LapinAsyncStdExt {
     where
         Self: Sized;
 
-    fn with_async_std_reactor(self) -> Self
-    where
-        Self: Sized;
+    //    fn with_async_std_reactor(self) -> Self
+    //    where
+    //        Self: Sized;
 }
 
 impl LapinAsyncStdExt for ConnectionProperties {
     fn with_async_std(self) -> Self {
-        self.with_async_std_executor().with_async_std_reactor()
+        self.with_async_std_executor() //.with_async_std_reactor()
     }
 
     fn with_async_std_executor(self) -> Self {
         self.with_executor(AsyncStdExecutor)
     }
 
-    fn with_async_std_reactor(self) -> Self {
-        // async-std uses smol underneath, use smol reactor until async-std exposes its own API
-        self.with_smol_reactor()
-    }
+    //    fn with_async_std_reactor(self) -> Self {
+    // async-std uses smol underneath, use smol reactor until async-std exposes its own API
+    //        self.with_smol_reactor()
+    //    }
 }
 
 #[derive(Debug)]
