amqp-rs/lapin

commit f7a125892a5a5b0c7a29f94c8715d4be848ce6c7

Channel: refactor receiver state handling
Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

src/channel.rs
@@ -1,7 +1,7 @@
 use crate::{
     acknowledgement::{Acknowledgements, DeliveryTag},
     auth::Credentials,
-    channel_status::{ChannelReceiverState, ChannelState, ChannelStatus},
+    channel_status::{ChannelState, ChannelStatus},
     close_on_drop,
     connection_status::{ConnectionState, ConnectionStep},
     consumer::Consumer,
@@ -126,10 +126,6 @@ impl Channel {
 
     pub(crate) fn set_state(&self, state: ChannelState) {
         self.status.set_state(state);
-    }
-
-    pub(crate) fn set_receiver_state(&self, state: ChannelReceiverState) {
-        self.status.set_receiver_state(state);
     }
 
     pub fn id(&self) -> u16 {
@@ -296,119 +292,65 @@ impl Channel {
         size: u64,
         properties: BasicProperties,
     ) -> Result<()> {
-        if let ChannelReceiverState::WillReceiveContent(
-            expected_class_id,
-            queue_name,
-            request_id_or_consumer_tag,
-        ) = self.status.receiver_state()
-        {
-            if class_id != expected_class_id {
-                error!(
-                    "received content header with class_id {} instead of expected {}",
-                    class_id, expected_class_id
-                );
-                let error = AMQPError::new(
-                        AMQPHardError::FRAMEERROR.into(),
-                        format!("content header frame with class_id {} instead of {} received on channel {}", class_id, expected_class_id, self.id).into(),
-                    );
+        self.status.set_content_length(
+            self.id,
+            class_id,
+            size as usize,
+            |queue_name, request_id_or_consumer_tag| {
+                if let Some(queue_name) = queue_name {
+                    self.queues.handle_content_header_frame(
+                        queue_name.as_str(),
+                        request_id_or_consumer_tag.clone(),
+                        size,
+                        properties,
+                    )?;
+                } else {
+                    self.returned_messages.set_delivery_properties(properties);
+                    if size == 0 {
+                        self.returned_messages
+                            .new_delivery_complete(self.status.confirm());
+                    }
+                }
+                Ok(())
+            },
+            |msg| {
+                error!("{}", msg);
+                let error = AMQPError::new(AMQPHardError::FRAMEERROR.into(), msg.into());
                 self.internal_rpc.close_connection(
                     error.get_id(),
                     error.get_message().to_string(),
                     class_id,
                     0,
                 );
-                return self.set_error(Error::ProtocolError(error));
-            }
-            if size > 0 {
-                self.set_receiver_state(ChannelReceiverState::ReceivingContent(
-                    queue_name.clone(),
-                    request_id_or_consumer_tag.clone(),
-                    size as usize,
-                ));
-            } else {
-                self.set_receiver_state(ChannelReceiverState::Idle);
-            }
-            if let Some(queue_name) = queue_name {
-                self.queues.handle_content_header_frame(
-                    queue_name.as_str(),
-                    request_id_or_consumer_tag,
-                    size,
-                    properties,
-                )?;
-            } else {
-                self.returned_messages.set_delivery_properties(properties);
-                if size == 0 {
-                    self.returned_messages
-                        .new_delivery_complete(self.status.confirm());
-                }
-            }
-            Ok(())
-        } else {
-            self.handle_invalid_contents(
-                format!(
-                    "unexpected content header frame received on channel {}",
-                    self.id
-                ),
-                class_id,
-                0,
-            )
-        }
+                self.set_error(Error::ProtocolError(error))
+            },
+            |msg| self.handle_invalid_contents(msg, class_id, 0),
+        )
     }
 
     pub(crate) fn handle_body_frame(&self, payload: Vec<u8>) -> Result<()> {
-        let payload_size = payload.len();
-
-        if let ChannelReceiverState::ReceivingContent(
-            queue_name,
-            request_id_or_consumer_tag,
-            remaining_size,
-        ) = self.status.receiver_state()
-        {
-            if remaining_size >= payload_size {
-                if let Some(queue_name) = queue_name.as_ref() {
+        self.status.receive(
+            self.id,
+            payload.len(),
+            |queue_name, request_id_or_consumer_tag, remaining_size| {
+                if let Some(queue_name) = queue_name {
                     self.queues.handle_body_frame(
                         queue_name.as_str(),
                         request_id_or_consumer_tag.clone(),
                         remaining_size,
-                        payload_size,
                         payload,
                     )?;
                 } else {
                     self.returned_messages.receive_delivery_content(payload);
-                    if remaining_size == payload_size {
+                    if remaining_size == 0 {
                         self.returned_messages
                             .new_delivery_complete(self.status.confirm());
                     }
                 }
-                if remaining_size == payload_size {
-                    self.set_receiver_state(ChannelReceiverState::Idle);
-                } else {
-                    self.set_receiver_state(ChannelReceiverState::ReceivingContent(
-                        queue_name,
-                        request_id_or_consumer_tag,
-                        remaining_size - payload_size,
-                    ));
-                }
                 Ok(())
-            } else {
-                self.handle_invalid_contents(
-                    format!(
-                        "unexpectedly large content body frame received on channel {} ({} bytes, expected {} bytes)",
-                        self.id, payload_size, remaining_size
-                    ),
-                    0, 0,
-                )
-            }
-        } else {
-            self.handle_invalid_contents(
-                format!(
-                    "unexpected content body frame received on channel {}",
-                    self.id
-                ),
-                0,
-                0,
-            )
-        }
+            },
+            |msg| self.handle_invalid_contents(msg, 0, 0),
+        )
     }
 
     fn before_basic_publish(&self) -> Option<PublisherConfirm> {
@@ -820,11 +762,7 @@ impl Channel {
             ),
             resolver,
         );
-        self.set_receiver_state(ChannelReceiverState::WillReceiveContent(
-            class_id,
-            Some(queue),
-            None,
-        ));
+        self.status.set_will_receive(class_id, Some(queue), None);
         Ok(())
     }
 
@@ -866,11 +804,8 @@ impl Channel {
                 method.redelivered,
             ),
         ) {
-            self.set_receiver_state(ChannelReceiverState::WillReceiveContent(
-                class_id,
-                Some(queue_name),
-                Some(method.consumer_tag),
-            ));
+            self.status
+                .set_will_receive(class_id, Some(queue_name), Some(method.consumer_tag));
         }
         Ok(())
     }
@@ -961,9 +896,7 @@ impl Channel {
                 method.reply_code,
                 method.reply_text,
             ));
-        self.set_receiver_state(ChannelReceiverState::WillReceiveContent(
-            class_id, None, None,
-        ));
+        self.status.set_will_receive(class_id, None, None);
         Ok(())
     }
 

src/channel_receiver_state.rs
@@ -1,0 +1,114 @@
+use crate::{
+    types::{ShortString, ShortUInt},
+    Result,
+};
+use std::collections::VecDeque;
+
+#[derive(Debug, Default)]
+pub(crate) struct ChannelReceiverStates(VecDeque<ChannelReceiverState>);
+
+impl ChannelReceiverStates {
+    #[cfg(test)]
+    pub(crate) fn receiver_state(&self) -> ChannelReceiverState {
+        self.0.front().unwrap().clone()
+    }
+
+    pub(crate) fn set_will_receive(
+        &mut self,
+        class_id: ShortUInt,
+        queue_name: Option<ShortString>,
+        request_id_or_consumer_tag: Option<ShortString>,
+    ) {
+        self.0.push_back(ChannelReceiverState::WillReceiveContent(
+            class_id,
+            queue_name,
+            request_id_or_consumer_tag,
+        ));
+    }
+
+    pub(crate) fn set_content_length<
+        Handler: FnOnce(&Option<ShortString>, &Option<ShortString>) -> Result<()>,
+        OnInvalidClass: FnOnce(String) -> Result<()>,
+        OnError: FnOnce(String) -> Result<()>,
+    >(
+        &mut self,
+        channel_id: u16,
+        class_id: ShortUInt,
+        length: usize,
+        handler: Handler,
+        invalid_class_hanlder: OnInvalidClass,
+        error_handler: OnError,
+    ) -> Result<()> {
+        if let Some(ChannelReceiverState::WillReceiveContent(
+            expected_class_id,
+            queue_name,
+            request_id_or_consumer_tag,
+        )) = self.0.pop_front()
+        {
+            if expected_class_id == class_id {
+                let res = handler(&queue_name, &request_id_or_consumer_tag);
+                if length > 0 {
+                    self.0.push_front(ChannelReceiverState::ReceivingContent(
+                        queue_name,
+                        request_id_or_consumer_tag,
+                        length,
+                    ));
+                }
+                res
+            } else {
+                invalid_class_hanlder(format!(
+                    "content header frame with class id {} instead of {} received on channel {}",
+                    class_id, expected_class_id, channel_id
+                ))
+            }
+        } else {
+            error_handler(format!(
+                "unexpected content header frame received on channel {}",
+                channel_id
+            ))
+        }
+    }
+
+    pub(crate) fn receive<
+        Handler: FnOnce(&Option<ShortString>, &Option<ShortString>, usize) -> Result<()>,
+        OnError: FnOnce(String) -> Result<()>,
+    >(
+        &mut self,
+        channel_id: u16,
+        length: usize,
+        handler: Handler,
+        error_handler: OnError,
+    ) -> Result<()> {
+        if let Some(ChannelReceiverState::ReceivingContent(
+            queue_name,
+            request_id_or_consumer_tag,
+            len,
+        )) = self.0.pop_front()
+        {
+            if let Some(remaining) = len.checked_sub(length) {
+                let res = handler(&queue_name, &request_id_or_consumer_tag, remaining);
+                if remaining > 0 {
+                    self.0.push_front(ChannelReceiverState::ReceivingContent(
+                        queue_name,
+                        request_id_or_consumer_tag,
+                        remaining,
+                    ));
+                }
+                res
+            } else {
+                error_handler(format!("unexpectedly large content body frame received on channel {} ({} ybtes, expected {} bytes)", channel_id, length, len))
+            }
+        } else {
+            error_handler(format!(
+                "unexpected content body frame received on channel {}",
+                channel_id
+            ))
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub(crate) enum ChannelReceiverState {
+    WillReceiveContent(ShortUInt, Option<ShortString>, Option<ShortString>),
+    ReceivingContent(Option<ShortString>, Option<ShortString>, usize),
+}

src/channel_status.rs
@@ -1,4 +1,8 @@
-use crate::types::{ShortString, ShortUInt};
+use crate::{
+    channel_receiver_state::ChannelReceiverStates,
+    types::{ShortString, ShortUInt},
+    Result,
+};
 use log::trace;
 use parking_lot::Mutex;
 use std::{fmt, sync::Arc};
@@ -42,12 +46,61 @@ impl ChannelStatus {
         self.inner.lock().state = state
     }
 
-    pub fn receiver_state(&self) -> ChannelReceiverState {
-        self.inner.lock().receiver_state.clone()
+    #[cfg(test)]
+    pub(crate) fn receiver_state(&self) -> crate::channel_receiver_state::ChannelReceiverState {
+        self.inner.lock().receiver_state.receiver_state()
     }
 
-    pub(crate) fn set_receiver_state(&self, state: ChannelReceiverState) {
-        self.inner.lock().receiver_state = state
+    pub(crate) fn set_will_receive(
+        &self,
+        class_id: ShortUInt,
+        queue_name: Option<ShortString>,
+        request_id_or_consumer_tag: Option<ShortString>,
+    ) {
+        self.inner.lock().receiver_state.set_will_receive(
+            class_id,
+            queue_name,
+            request_id_or_consumer_tag,
+        );
+    }
+
+    pub(crate) fn set_content_length<
+        Handler: FnOnce(&Option<ShortString>, &Option<ShortString>) -> Result<()>,
+        OnInvalidClass: FnOnce(String) -> Result<()>,
+        OnError: FnOnce(String) -> Result<()>,
+    >(
+        &self,
+        channel_id: u16,
+        class_id: ShortUInt,
+        length: usize,
+        handler: Handler,
+        invalid_class_hanlder: OnInvalidClass,
+        error_handler: OnError,
+    ) -> Result<()> {
+        self.inner.lock().receiver_state.set_content_length(
+            channel_id,
+            class_id,
+            length,
+            handler,
+            invalid_class_hanlder,
+            error_handler,
+        )
+    }
+
+    pub(crate) fn receive<
+        Handler: FnOnce(&Option<ShortString>, &Option<ShortString>, usize) -> Result<()>,
+        OnError: FnOnce(String) -> Result<()>,
+    >(
+        &self,
+        channel_id: u16,
+        length: usize,
+        handler: Handler,
+        error_handler: OnError,
+    ) -> Result<()> {
+        self.inner
+            .lock()
+            .receiver_state
+            .receive(channel_id, length, handler, error_handler)
     }
 
     pub(crate) fn set_send_flow(&self, flow: bool) {
@@ -74,19 +127,6 @@ impl Default for ChannelState {
     }
 }
 
-#[derive(Clone, Debug, PartialEq, Eq)]
-pub enum ChannelReceiverState {
-    Idle,
-    WillReceiveContent(ShortUInt, Option<ShortString>, Option<ShortString>),
-    ReceivingContent(Option<ShortString>, Option<ShortString>, usize),
-}
-
-impl Default for ChannelReceiverState {
-    fn default() -> Self {
-        ChannelReceiverState::Idle
-    }
-}
-
 impl fmt::Debug for ChannelStatus {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let mut debug = f.debug_struct("ChannelStatus");
@@ -105,7 +145,7 @@ struct Inner {
     confirm: bool,
     send_flow: bool,
     state: ChannelState,
-    receiver_state: ChannelReceiverState,
+    receiver_state: ChannelReceiverStates,
 }
 
 impl Default for Inner {
@@ -114,7 +154,7 @@ impl Default for Inner {
             confirm: false,
             send_flow: true,
             state: ChannelState::default(),
-            receiver_state: ChannelReceiverState::default(),
+            receiver_state: ChannelReceiverStates::default(),
         }
     }
 }

src/connection.rs
@@ -284,7 +284,8 @@ mod tests {
     use env_logger;
 
     use super::*;
-    use crate::channel_status::{ChannelReceiverState, ChannelState};
+    use crate::channel_receiver_state::ChannelReceiverState;
+    use crate::channel_status::ChannelState;
     use crate::types::ShortString;
     use crate::BasicProperties;
     use amq_protocol::frame::AMQPContentHeader;

src/lib.rs
@@ -131,6 +131,7 @@ type PromiseResolver<T> = pinky_swear::Pinky<Result<T>>;
 mod acknowledgement;
 mod buffer;
 mod channel;
+mod channel_receiver_state;
 mod channel_status;
 mod channels;
 mod close_on_drop;

src/queues.rs
@@ -139,7 +139,6 @@ impl Queues {
         queue: &str,
         consumer_tag: Option<ShortString>,
         remaining_size: usize,
-        payload_size: usize,
         payload: Vec<u8>,
     ) -> Result<()> {
         self.with_queue(queue, |queue| {
@@ -147,14 +146,14 @@ impl Queues {
                 Some(consumer_tag) => {
                     if let Some(consumer) = queue.get_consumer(&consumer_tag) {
                         consumer.receive_delivery_content(payload);
-                        if remaining_size == payload_size {
+                        if remaining_size == 0 {
                             consumer.new_delivery_complete()?;
                         }
                     }
                 }
                 None => {
                     queue.receive_delivery_content(payload);
-                    if remaining_size == payload_size {
+                    if remaining_size == 0 {
                         queue.new_delivery_complete();
                     }
                 }

commit 5d5afb1ed2dc57fcece79e50d07fa002e6bf09b9

avoid deadlock when receiving returned message
Regression from f7a125892a5a5b0c7a29f94c8715d4be848ce6c7

Also add the publisher_confirms example as a test to avoid further
regressions

Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

src/channel.rs
@@ -312,6 +312,7 @@ impl Channel {
         size: u64,
         properties: BasicProperties,
     ) -> Result<()> {
+        let confirm = self.status.confirm();
         self.status.set_content_length(
             self.id,
             class_id,
@@ -329,7 +330,7 @@ impl Channel {
                     self.returned_messages.set_delivery_properties(properties);
                     if size == 0 {
                         self.returned_messages
-                            .new_delivery_complete(self.status.confirm());
+                            .new_delivery_complete(confirm);
                     }
                 }
                 Ok(())
@@ -350,6 +351,7 @@ impl Channel {
     }
 
     pub(crate) fn handle_body_frame(&self, payload: Vec<u8>) -> Result<()> {
+        let confirm = self.status.confirm();
         self.status.receive(
             self.id,
             payload.len(),
@@ -366,7 +368,7 @@ impl Channel {
                     self.returned_messages.receive_delivery_content(payload);
                     if remaining_size == 0 {
                         self.returned_messages
-                            .new_delivery_complete(self.status.confirm());
+                            .new_delivery_complete(confirm);
                     }
                 }
                 Ok(())

tests/publisher_confirms.rs
@@ -1,0 +1,1 @@
+../examples/publisher_confirms.rs