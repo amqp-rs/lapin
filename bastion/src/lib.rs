299885cf7f664abe9ee8ef6760fcc3095044b9c3
Luca Palmieri <lpalmieri@truelayer.com>
2020-06-23 21:55:17 +0100

    Add bastion executor implementation.

---
+++ bastion/src/lib.rs
@@ -0,0 +1,25 @@
+use lapin::{executor::Executor, ConnectionProperties};
+use std::{future::Future, pin::Pin};
+use bastion::{spawn};
+
+pub trait BastionExt {
+    fn with_bastion(self) -> Self
+        where
+            Self: Sized;
+}
+
+impl BastionExt for ConnectionProperties {
+    fn with_bastion(self) -> Self {
+        self.with_executor(BastionExecutor)
+    }
+}
+
+#[derive(Debug)]
+struct BastionExecutor;
+
+impl Executor for BastionExecutor {
+    fn spawn(&self, f: Pin<Box<dyn Future<Output = ()> + Send>>) -> Result<(), lapin::Error> {
+        spawn!(f);
+        Ok(())
+    }
+}
