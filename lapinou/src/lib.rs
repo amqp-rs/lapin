0919bfd744506eca4ba2665d27b36061a50f6358
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-07-20 10:57:12 +0200

    lapinou: switch to async-lapin
    but wait for smol 0.2 before issuing a release
    
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- lapinou/src/lib.rs
+++ lapinou/src/lib.rs
@@ -1,14 +1,7 @@
-use lapin::{
-    executor::Executor,
-    heartbeat::Heartbeat,
-    reactor::{Reactor, ReactorBuilder, ReactorHandle, Slot},
-    socket_state::{SocketEvent, SocketStateHandle},
-    tcp::{TcpStream, TcpStreamWrapper},
-    ConnectionProperties, Result,
-};
-use simple_mutex::Mutex;
-use smol::{Async, Task, Timer};
-use std::{collections::HashMap, fmt, future::Future, pin::Pin, sync::Arc};
+use async_lapin::*;
+use lapin::{executor::Executor, ConnectionProperties, Result};
+use smol::Task;
+use std::{future::Future, pin::Pin};
 
 // ConnectionProperties extension
 
@@ -35,7 +28,7 @@ impl LapinSmolExt for ConnectionProperties {
     }
 
     fn with_smol_reactor(self) -> Self {
-        self.with_reactor(SmolReactorBuilder)
+        self.with_async_io_reactor(|fut| Task::spawn(fut).detach())
     }
 }
 
@@ -49,116 +42,4 @@ impl Executor for SmolExecutor {
         Task::spawn(f).detach();
         Ok(())
     }
-}
-
-// Reactor
-
-#[derive(Debug)]
-struct SmolReactorBuilder;
-
-#[derive(Debug)]
-struct SmolReactor(SmolReactorHandle);
-
-#[derive(Clone)]
-struct SmolReactorHandle {
-    heartbeat: Heartbeat,
-    inner: Arc<Mutex<Inner>>,
-}
-
-#[derive(Default)]
-struct Inner {
-    slot: Slot,
-    slots: HashMap<usize, (Arc<Async<TcpStreamWrapper>>, SocketStateHandle)>,
-}
-
-impl fmt::Debug for SmolReactorHandle {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("SmolReactorHandle").finish()
-    }
-}
-
-impl Inner {
-    fn register(
-        &mut self,
-        socket: Arc<Async<TcpStreamWrapper>>,
-        socket_state: SocketStateHandle,
-    ) -> Result<usize> {
-        let slot = self.slot;
-        self.slot += 1;
-        self.slots.insert(slot, (socket, socket_state));
-        Ok(slot)
-    }
-}
-
-impl ReactorBuilder for SmolReactorBuilder {
-    fn build(&self, heartbeat: Heartbeat) -> Result<Box<dyn Reactor + Send>> {
-        Ok(Box::new(SmolReactor(SmolReactorHandle {
-            heartbeat,
-            inner: Arc::new(Mutex::new(Inner::default())),
-        })))
-    }
-}
-
-impl Reactor for SmolReactor {
-    fn register(
-        &mut self,
-        socket: &mut TcpStream,
-        socket_state: SocketStateHandle,
-    ) -> Result<usize> {
-        let socket = Arc::new(Async::new(unsafe { TcpStreamWrapper::new(socket) })?);
-        let slot = self.0.inner.lock().register(socket, socket_state)?;
-        self.0.poll_read(slot);
-        self.0.poll_write(slot);
-        Ok(slot)
-    }
-
-    fn handle(&self) -> Box<dyn ReactorHandle + Send> {
-        Box::new(self.0.clone())
-    }
-}
-
-impl ReactorHandle for SmolReactorHandle {
-    fn start_heartbeat(&self) {
-        Task::spawn(heartbeat(self.heartbeat.clone())).detach();
-    }
-
-    fn poll_read(&self, slot: usize) {
-        if let Some((socket, socket_state)) = self.inner.lock().slots.get(&slot) {
-            Task::spawn(poll_read(socket.clone(), socket_state.clone()))
-                .unwrap()
-                .detach();
-        }
-    }
-
-    fn poll_write(&self, slot: usize) {
-        if let Some((socket, socket_state)) = self.inner.lock().slots.get(&slot) {
-            Task::spawn(poll_write(socket.clone(), socket_state.clone()))
-                .unwrap()
-                .detach();
-        }
-    }
-}
-
-async fn heartbeat(heartbeat: Heartbeat) {
-    while let Ok(Some(timeout)) = heartbeat.poll_timeout() {
-        Timer::after(timeout).await;
-    }
-}
-
-async fn poll_read(
-    socket: Arc<Async<TcpStreamWrapper>>,
-    socket_state: SocketStateHandle,
-) -> Result<()> {
-    socket.readable().await?;
-    socket_state.send(SocketEvent::Readable);
-    Ok(())
-}
-
-async fn poll_write(
-    socket: Arc<Async<TcpStreamWrapper>>,
-    socket_state: SocketStateHandle,
-) -> Result<()> {
-    socket.writable().await?;
-    socket_state.send(SocketEvent::Writable);
-    Ok(())
 }
