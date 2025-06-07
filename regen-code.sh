a6d8e1fcd7ee2a6be802adba66fb6bfeff73bd37
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2025-06-07 16:55:19 +0200

    format generated code with edition 2024 too

--- regen-code.sh
+++ regen-code.sh
@@ -5,7 +5,7 @@ main() {
     export LAPIN_CODEGEN_FILE="generated"
 
     cargo build --features=codegen-internal
-    rustfmt "${LAPIN_CODEGEN_DIR}/${LAPIN_CODEGEN_FILE}.rs" --edition=2021
+    rustfmt "${LAPIN_CODEGEN_DIR}/${LAPIN_CODEGEN_FILE}.rs" --edition=2024
 }
 
 main "${@}"
