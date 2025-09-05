01af99b586afc1a55f408221437f0180daf1c393
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2025-09-05 20:47:21 +0200

    ConnectionStatus: switch to RwLock

--- src/connection_status.rs
+++ src/connection_status.rs
@@ -5,11 +5,11 @@ use crate::{
 use amq_protocol::auth::{Credentials, SASLMechanism};
 use std::{
     fmt,
-    sync::{Arc, Mutex, MutexGuard},
+    sync::{Arc, RwLock, RwLockReadGuard, RwLockWriteGuard},
 };
 
 #[derive(Clone, Default)]
-pub struct ConnectionStatus(Arc<Mutex<Inner>>);
+pub struct ConnectionStatus(Arc<RwLock<Inner>>);
 
 impl ConnectionStatus {
     pub(crate) fn new(uri: &AMQPUri) -> Self {
@@ -20,11 +20,11 @@ impl ConnectionStatus {
     }
 
     pub(crate) fn state(&self) -> ConnectionState {
-        self.lock_inner().state
+        self.read().state
     }
 
     pub(crate) fn set_state(&self, state: ConnectionState) -> ConnectionState {
-        let mut inner = self.lock_inner();
+        let mut inner = self.write();
         std::mem::replace(&mut inner.state, state)
     }
 
@@ -37,58 +37,58 @@ impl ConnectionStatus {
         mechanism: SASLMechanism,
         options: ConnectionProperties,
     ) -> Result<()> {
-        self.lock_inner()
+        self.write()
             .set_connecting(resolver_out, resolver_in, conn, creds, mechanism, options)
     }
 
     pub(crate) fn set_reconnecting(&self) {
-        self.lock_inner().set_reconnecting();
+        self.write().set_reconnecting();
     }
 
     pub(crate) fn connection_step(&self) -> Option<ConnectionStep> {
-        self.lock_inner().connection_step.take()
+        self.write().connection_step.take()
     }
 
     pub(crate) fn set_connection_step(&self, connection_step: ConnectionStep) {
-        self.lock_inner().connection_step = Some(connection_step);
+        self.write().connection_step = Some(connection_step);
     }
 
     pub(crate) fn connection_resolver(&self) -> Option<PromiseResolver<Connection>> {
-        let resolver = self.lock_inner().connection_resolver();
+        let resolver = self.write().connection_resolver();
         // We carry the Connection here to drop the lock() above before dropping the Connection
         resolver.map(|(resolver, _connection)| resolver.resolver_in)
     }
 
     pub fn vhost(&self) -> String {
-        self.lock_inner().vhost.clone()
+        self.read().vhost.clone()
     }
 
     pub(crate) fn set_vhost(&self, vhost: &str) {
-        self.lock_inner().vhost = vhost.into();
+        self.write().vhost = vhost.into();
     }
 
     pub fn username(&self) -> String {
-        self.lock_inner().username.clone()
+        self.read().username.clone()
     }
 
     pub(crate) fn set_username(&self, username: &str) {
-        self.lock_inner().username = username.into();
+        self.write().username = username.into();
     }
 
     pub(crate) fn block(&self) {
-        self.lock_inner().blocked = true;
+        self.write().blocked = true;
     }
 
     pub(crate) fn unblock(&self) {
-        self.lock_inner().blocked = false;
+        self.write().blocked = false;
     }
 
     pub fn blocked(&self) -> bool {
-        self.lock_inner().blocked
+        self.read().blocked
     }
 
     pub fn connected(&self) -> bool {
-        self.lock_inner().state == ConnectionState::Connected
+        self.state() == ConnectionState::Connected
     }
 
     pub(crate) fn ensure_connected(&self) -> Result<()> {
@@ -99,35 +99,39 @@ impl ConnectionStatus {
     }
 
     pub fn connecting(&self) -> bool {
-        self.lock_inner().state == ConnectionState::Connecting
+        self.state() == ConnectionState::Connecting
     }
 
     pub fn reconnecting(&self) -> bool {
-        self.lock_inner().state == ConnectionState::Reconnecting
+        self.state() == ConnectionState::Reconnecting
     }
 
     pub fn closing(&self) -> bool {
-        self.lock_inner().state == ConnectionState::Closing
+        self.state() == ConnectionState::Closing
     }
 
     pub fn closed(&self) -> bool {
-        self.lock_inner().state == ConnectionState::Closed
+        self.state() == ConnectionState::Closed
     }
 
     pub fn errored(&self) -> bool {
-        self.lock_inner().state == ConnectionState::Error
+        self.state() == ConnectionState::Error
     }
 
     pub(crate) fn poison(&self, err: Error) {
-        self.lock_inner().poison(err);
+        self.write().poison(err);
     }
 
     pub(crate) fn auto_close(&self) -> bool {
-        [ConnectionState::Connecting, ConnectionState::Connected].contains(&self.lock_inner().state)
+        [ConnectionState::Connecting, ConnectionState::Connected].contains(&self.state())
     }
 
-    fn lock_inner(&self) -> MutexGuard<'_, Inner> {
-        self.0.lock().unwrap_or_else(|e| e.into_inner())
+    fn read(&self) -> RwLockReadGuard<'_, Inner> {
+        self.0.read().unwrap_or_else(|e| e.into_inner())
+    }
+
+    fn write(&self) -> RwLockWriteGuard<'_, Inner> {
+        self.0.write().unwrap_or_else(|e| e.into_inner())
     }
 }
 
@@ -199,7 +203,7 @@ pub enum ConnectionState {
 impl fmt::Debug for ConnectionStatus {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let mut debug = f.debug_struct("ConnectionStatus");
-        if let Ok(inner) = self.0.try_lock() {
+        if let Ok(inner) = self.0.try_read() {
             debug
                 .field("state", &inner.state)
                 .field("vhost", &inner.vhost)
